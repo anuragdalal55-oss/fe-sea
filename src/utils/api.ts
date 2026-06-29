5d796af97b00d0109061c2e91c1b5cacbd244915
Robin Maan <robin_maan@zykrr.com>
2026-06-29 10:55:01 +0530

    fix: issue local request

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BASE_URL = 'https://sea-be.onrender.com/api'
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL  || '/api',
+  baseURL: BASE_URL,
   headers: { 'Content-Type': 'application/json' },
 });
 
